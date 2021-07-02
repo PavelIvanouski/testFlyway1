0bc9fa2dcf684371b046670f588fbfb961ea956d
Pavel Ivanouski <pavel_85_67@mail.ru>
2021-07-02 16:19:07 +0200

    initial commit

---
+++ src/main/java/com/example/testFlyway1/TestFlyway1Application.java
@@ -0,0 +1,13 @@
+package com.example.testFlyway1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFlyway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFlyway1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testFlyway1/TestFlyway1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testFlyway1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFlyway1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
